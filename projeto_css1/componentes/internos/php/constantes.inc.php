7ead6b2511c5e766040040b085bfbe6170da79a3
mfpaulino <mfpaulino@uol.com.br>
2017-08-19 12:30:13 -0300

    home 19_08 renomeio index_

--- projeto_css1/componentes/internos/php/constantes.inc.php
+++ projeto_css1/componentes/internos/php/constantes.inc.php
@@ -3,8 +3,8 @@
 
 define('TEMPO_MAX_INATIVIDADE', 180);//tempo em segundos
 define('TITULO','SIAUD-EB');
-define('PAGINA_INICIAL','index_user.php');
-define('PAGINA_VISITANTE','index_visite.php');
+define('PAGINA_INICIAL','user.php');
+define('PAGINA_VISITANTE','guess.php');
 define('ACESSO_NEGADO', 'acesso_negado.php');
 define('PAGINA_BLOQUEIO', 'tela_bloqueio.php');
 define('HOME', '//localhost/desenvolvimento/intranet/servidor_mamba/seb/projeto_css1/');
