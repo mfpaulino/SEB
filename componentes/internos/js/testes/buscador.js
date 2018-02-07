d9ccd8ca8c28be36f26a126e0d6d14f7e4199188
mfpaulino <mfpaulino@uol.com.br>
2018-02-07 12:32:51 -0200

    7_02_18

---
+++ componentes/internos/js/testes/buscador.js
@@ -0,0 +1,24 @@
+//buscador sem refresh
+function buscar(palavra)
+{
+var page = "controllers/testes/buscador.inc.php";
+$.ajax
+        ({
+            type: 'POST',
+            dataType: 'html',
+            url: page,
+            beforeSend: function () {
+                $("#dados").html("Carregando...");
+            },
+            data: {palavra: palavra},
+            success: function (msg)
+            {
+                $("#dados").html(msg);
+            }
+        });
+}
+
+
+$('#buscar').click(function () {
+buscar($("#palavra").val())
+});
